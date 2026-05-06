8664f8655930a6660b628b8543f7be54a2c0d61d
strikeout2nt <149441196+strikeout2nt@users.noreply.github.com>
2026-05-06 03:50:37 +0000

    abstrast PB and TPB made

---
+++ PhoneBook.java
@@ -0,0 +1,88 @@
+public abstract class PhoneBook{
+
+    private int areaCode;
+    private int remDigits;
+    private String name;
+    private String job;
+
+    public PhoneBook(int area, int rDigits, String n, String j){
+        setAreaCode(areaCode);
+        setRemDigits(remDigits);
+        setName(name);
+        setJob(job);
+    }
+
+    public void setAreaCode(int areaCode){
+        validateAreaCode(areaCode);
+        this.areaCode = areaCode;
+    
+    }
+
+    protected void validateAreaCode(int areaCode) {
+        if (areaCode < 100 || areaCode > 999) {
+            throw new IllegalArgumentException("Area code must be 3 digits");
+        }
+    }
+
+    public void setRemDigits(int remDigits){
+        validateRemDigits(remDigits);
+        this.remDigits = remDigits;
+    }
+
+    protected void validateRemDigits(int remDigits){
+    //1_000_000 smallest 7 digit and 9_999_999 largest 7 digit
+        if (remDigits < 1_000_000 || remDigits > 9_999_999) {
+        throw new IllegalArgumentException("Remaining digits must be exactly 7 digits");
+        }
+    }
+
+    public void setName(String name){
+        validateName(name);
+        this.name = name;
+    }
+
+    protected void validateName(String name){
+        if(name == " "){
+            throw new IllegalArgumentException("Name must not be empty");
+        }
+    }
+
+    public void setJob(String job){
+        validateJob(job);
+        this.job = job;
+    }
+
+    protected void validateJob(String job){
+        if(job == " "){
+            throw new IllegalArgumentException("Job must not be empty");
+        }
+    }
+
+    public int getAreaCode(){
+        return areaCode;
+    }
+
+    public int getremDigits(){
+        return remDigits;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getJob(){
+        return job;
+    }
+
+    //formatting for WhereFrom methods
+    public String formatPhoneNumber() {
+            String digits = String.format("%07d", getremDigits());
+            return "(" + getAreaCode() + ") " 
+                + digits.substring(0, 3) + "-" 
+                + digits.substring(3);
+    }
+
+    public void WhereFrom(){
+
+    }
+}
---
+++ TexasPhoneBook.java
@@ -0,0 +1,54 @@
+public class TexasPhoneBook extends PhoneBook{
+
+    
+        private static final int[] TEXAS_CODES = {
+        210, 214, 254, 281, 325, 346, 361, 409, 430,
+        432, 469, 512, 621, 682, 713, 726, 737, 806,
+        817, 830, 832, 903, 915, 936, 940, 945, 956,
+        972, 979};
+
+        public TexasPhoneBook(int areaCode, int remDigits, String name, String job){
+        super(areaCode, remDigits, name, job);
+
+    
+        }
+
+        @Override
+        protected void validateAreaCode(int areaCode) {
+        for (int code : TEXAS_CODES) {
+            if (areaCode == code)
+                return;  // checks if valid
+        }
+        throw new IllegalArgumentException("Invalid Texas area code");
+        }
+
+
+        @Override
+        public void WhereFrom() {
+
+            String info = formatPhoneNumber() + " — " 
+                        + getName() + " (" + getJob() + ") ";
+
+            switch (getAreaCode()) {
+
+                case 214: case 469: case 972:
+                    System.out.println(info + "lives in Dallas, Texas");
+                    break;
+
+                case 713: case 281: case 832:
+                    System.out.println(info + "lives in Houston, Texas");
+                    break;
+
+                case 512: case 737:
+                    System.out.println(info + "lives in Austin, Texas");
+                    break;
+
+                case 956:
+                    System.out.println(info + "lives in the Rio Grande Valley, Texas");
+                    break;
+
+                default:
+                    System.out.println(info + "lives somewhere in Texas");
+        }
+    }
+}
